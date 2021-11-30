a647495e7c6eb74b515b766d79af7fc33f51abd2
Scott Gee <Scott.Gee@JohnLewis.co.uk>
2021-11-30 17:51:04 +0000

    Add comments to day 1

--- 2020/day-1/solution.R
+++ 2020/day-1/solution.R
@@ -4,25 +4,24 @@ library(here)
 
 source(here('2020', 'day-1', 'data.R'))
 
-tibble(value = list) %>% 
+tibble(value = list) %>%  # Create tibble from list of numbers
   expand(value, 
-         value1 = value) %>% 
-  filter(value < value1) %>% 
-  mutate(sum = value + value1) %>% 
-  filter(sum == 2020) %>% 
-  mutate(product = value * value1) %>% 
-  pull(product)
+         value1 = value) %>% # Get every combination of numbers in two columns
+  filter(value < value1) %>% # Remove duplicates
+  mutate(sum = value + value1) %>%  # Add together
+  filter(sum == 2020) %>% # Keep rows that sum to 2020
+  mutate(product = value * value1) %>% # Multiply columns
+  pull(product) # Get answer
 
-tibble(value = list) %>% 
+tibble(value = list) %>% # Create tibble from list of numbers
   expand(value,
          value1 = value,
-         value2 = value) %>% 
+         value2 = value) %>% # Get every combination of numbers in three columns
   filter(value < value1,
          value < value2,
-         value1 < value2) %>% 
-  mutate(sum = value + value1 + value2) %>% 
-  filter(sum == 2020) %>% 
+         value1 < value2) %>% # Remove duplicates
+  mutate(sum = value + value1 + value2) %>% # Add together
+  filter(sum == 2020) %>% # Keep rows that sum to 2020
   mutate(product = value * value1 * value2) %>% 
-  pull(product)
-
+  pull(product) # Get answer
 
